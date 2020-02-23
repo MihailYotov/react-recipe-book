34d831bea0610d7721b3aee56597291e8427440c
Mihail Yotov <myjubox@gmail.com>
2020-02-23 10:09:40 +0200

    Fetching the data from the server

--- src/containers/RecipesPage/RecipesPage.js
+++ src/containers/RecipesPage/RecipesPage.js
@@ -4,59 +4,13 @@ import classes from './RecipesPage.module.css';
 import RecipesList from '../../components/RecipesList/RecipesList';
 import RecipePreview from '../../components/RecipePreview/RecipePreview';
 import Navigation from "../../components/Navigation/Navigation";
+import axios from '../../axios-base';
 
 class RecipesPage extends Component {
-    recipesData = [
-        {
-            id: 0,
-            name: 'Soup',
-            ingredients: [
-                {ingredient: 'carrot', amount: 1, units: 'pcs'},
-                {ingredient: 'onion', amount: 1, units: 'pcs'},
-                {ingredient: 'pepper', amount: 1, units: 'pcs'},
-                {ingredient: 'garlic', amount: 1, units: 'pcs'},
-                {ingredient: 'tomato', amount: 1, units: 'pcs'},
-            ],
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        },
-        {
-            id: 1,
-            name: 'Salad',
-            ingredients: [
-                {ingredient: 'carrot', amount: 1, units: 'pcs'},
-                {ingredient: 'onion', amount: 1, units: 'pcs'},
-                {ingredient: 'pepper', amount: 1, units: 'pcs'},
-                {ingredient: 'garlic', amount: 1, units: 'pcs'},
-                {ingredient: 'tomato', amount: 1, units: 'pcs'},
-            ],
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        },
-        {
-            id: 2,
-            name: 'Roast',
-            ingredients: [
-                {ingredient: 'carrot', amount: 1, units: 'pcs'},
-                {ingredient: 'onion', amount: 1, units: 'pcs'},
-                {ingredient: 'Meat', amount: 0.5, units: 'kg'},
-                {ingredient: 'garlic', amount: 1, units: 'pcs'},
-                {ingredient: 'tomato', amount: 1, units: 'pcs'},
-            ],
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        },
-        {
-            id: 3,
-            name: 'Spaghetti',
-            ingredients: [
-                {ingredient: 'spaghetti', amount: 1, units: 'box'},
-                {ingredient: 'onion', amount: 1, units: 'pcs'},
-                {ingredient: 'carrot', amount: 1, units: 'pcs'},
-                {ingredient: 'garlic', amount: 1, units: 'pcs'},
-                {ingredient: 'tomato souse', amount: 1, units: 'can'},
-            ],
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        },
-
-    ];
+    state = {
+        previewedRecipe: null,
+        recipesData: []
+    };
 
     recipesNavProps = [
         {
@@ -65,12 +19,24 @@ class RecipesPage extends Component {
         }
     ];
 
-    state = {
-        previewedRecipe: this.recipesData[0]
-    };
+    componentDidMount() {
+        axios.get('/recipes.json')
+            .then((results) => {
+                const fetchedData = [];
+
+                for (let key in results.data) {
+                    fetchedData.push(results.data[key]);
+                }
+
+                this.setState({recipesData: fetchedData, previewedRecipe: fetchedData[0]})
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
 
     previewRecipe = (index) => {
-        this.setState({previewedRecipe: this.recipesData[index]})
+        this.setState({previewedRecipe: this.state.recipesData[index]})
     };
 
     render() {
@@ -80,10 +46,17 @@ class RecipesPage extends Component {
                 <div className={classes.GridContainer}>
                     <div className={classes.RecipesList}>
                         <div><Navigation navProps={this.recipesNavProps}/></div>
-                        <RecipesList recipes={this.recipesData} previewRecipe={this.previewRecipe}/>
+                        {
+                            this.state.recipesData.length ?
+                                <RecipesList recipes={this.state.recipesData} previewRecipe={this.previewRecipe}/>
+                                : <div>You still haven't added any recipes</div>
+                        }
                     </div>
                     <div className={classes.RecipePreview}>
-                        <RecipePreview recipe={this.state.previewedRecipe}/>
+                        {this.state.previewedRecipe ?
+                            <RecipePreview recipe={this.state.previewedRecipe}/>
+                            : <div>Select recipe</div>
+                        }
                     </div>
                 </div>
             </Fragment>
